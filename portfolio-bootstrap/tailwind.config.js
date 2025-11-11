fb763923a58a03377165cb93c0e1e358e432d222
miguelzackk <mzsilvaw@gmail.com>
2025-11-11 11:11:41 -0300

    feat: start project

---
+++ portfolio-bootstrap/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', 
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      // É aqui que você colocará suas "variáveis"
+      // (cores, fontes, etc.) no futuro
+    },
+  },
+  plugins: [],
+}
