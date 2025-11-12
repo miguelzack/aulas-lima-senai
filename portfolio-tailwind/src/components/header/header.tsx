e1a3312537735860302627d5b4598339e44dc7cb
miguelzackk <mzsilvaw@gmail.com>
2025-11-12 10:19:47 -0300

    feat: finish header.

--- portfolio-tailwind/src/components/header/header.tsx
+++ portfolio-tailwind/src/components/header/header.tsx
@@ -2,7 +2,7 @@ import { ButtonLinkedin } from "../button-linkedin/button-link";
 
 export function Header() {
     return (
-        <header className="flex flex-row items-center justify-around w-full">
+        <header className="flex flex-row items-center justify-between w-full">
             <h2 className="text-[40px] font-extrabold">Miguel <span className="text-primary">Silva.</span>
             </h2>
             <nav className="flex py-[15px] px-[90px] gap-[75px] items-center justify-center rounded-[35px] bg-white shadow-[0_4px_15px_0_rgba(0,0,0,0.10)]">
