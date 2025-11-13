04ba866fc4741e4e0119c0ef11681b8493b251dc
miguelzackk <mzsilvaw@gmail.com>
2025-11-13 11:09:33 -0300

    feat: finish portfolio.

--- portfolio-tailwind/src/app/page.tsx
+++ portfolio-tailwind/src/app/page.tsx
@@ -5,6 +5,8 @@ import { Perfil, ProjectBusPoint, ProjectSenaiExchage } from "@/assets/images/ex
 import Image from "next/image";
 import ButtonDownload from "@/components/button-download/button-download";
 import { ProjectCard } from "@/components/project-card/project-card";
+import { ButtonLinkedin } from "@/components/button-linkedin/button-link";
+import { SendEmailIco } from "@/assets/svgs/export"
 
 export default function Home() {
   return (
@@ -62,6 +64,33 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className="flex flex-col items-start gap-2.5 self-stretch bg-primary">
+        <div className="flex py-[100px] justify-center items-center gap-[141px] self-stretch">
+          <div className="flex w-[45.572%] flex-col items-start gap-5">
+            <Image
+              src={Perfil}
+              alt="Minha foto de perfil"
+              className="w-[183px]"
+            />
+            <h5 className="text-white text-[40px] font-extrabold">Contact me</h5>
+            <p className="self-stretch text-xl font-normal text-white">Interested in discussing web development, sharing ideas about software projects, or exploring new solutions in programming? Feel free to reach out—let's connect and keep building.</p>
+            <ButtonLinkedin />
+          </div>
+          <div className="flex p-10 flex-col justify-center items-center gap-[25px] rounded-xl bg-white w-[23%]">
+            <h6 className="text-primary text-base font-extrabold">Let's Stay Connected</h6>
+            <form action="" method="post" className="flex flex-col items-start gap-5 self-stretch">
+              <input placeholder="Your Name*" type="text" id="name" className="flex w-full px-[25px] py-2.5 rounded-lg border border-border-form text-base text-font-form font-normal" />
+              <input placeholder="Email ID*" type="email" id="email" className="flex w-full px-[25px] py-2.5 rounded-lg border border-border-form text-base text-font-form font-normal" />
+              <input placeholder="Phone No*" type="number" id="number" className="flex w-full px-[25px] py-2.5 rounded-lg border border-border-form text-base text-font-form font-normal" />
+              <textarea name="message" id="message" placeholder="Message*" className="w-full h-[105px] py-2.5 px-[25px] items-start gap-2.5 rounded-lg border border-border-form text-base text-font-form font-normal"></textarea>
+              <button className="login flex py-[15px] px-[25px] justify-center items-center gap-2.5 rounded-[35px] border border-white bg-primary text-xl font-normal text-white" type="submit"> <SendEmailIco /> Send email</button>
+            </form>
+          </div>
+        </div>
+      </section>
+      <footer className="flex py-[50px] justify-center items-center gap-2.5 self-stretch bg-black">
+        <p className="text-gray-custom text-base font-normal">Copyright 2025 @CIT || All Rights Reserved</p>
+      </footer>
     </div>
   );
 }
